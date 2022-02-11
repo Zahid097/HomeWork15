2324f72f05a2434d430b50ba0bd2f08f36a7679e
Beksultan Mamatkadyr uulu <90905002+beksultanFromPeaksoft@users.noreply.github.com>
2022-02-11 17:32:52 +0600

    missed some stuff in Singer class

--- src/com/company/Singer.java
+++ src/com/company/Singer.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class Singer extends Person {
     String bandName;
-
-
+//no arg constructor!
     public Singer(String name, String designation, String bandName) {
         super(name, designation);
         this.bandName = bandName;
     }
+    
+    //you should define toString() method at last
     @Override
     public String toString(){
         return name +" "+ designation +" "+ bandName;
@@ -15,11 +16,11 @@ public class Singer extends Person {
 
     public void singing() {
         System.out.println("Yrdaganda tim ele onuncho");
-
     }
 
     public void playGitar() {
         System.out.println("Gitarany ezetda ezet");
-
     }
+   
+    //getter & setter!!!
 }
